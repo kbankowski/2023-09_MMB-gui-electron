f593811c63cdad50bbe28e363d72065ef3b52ba3
bankows <kristofer.bank@gmail.com>
2023-09-25 12:45:33 +0200

    Copying the two files ad-hoc files send by Alex

---
+++ mmb-electron.app/Contents/Resources/app/dist/electron/static/mmci-cli/mmb_2.m
@@ -0,0 +1,147 @@
+clear all
+close all
+clc
+clear all
+
+%% Figure 3
+mmb('config_2.json','var');
+
+cd out
+
+%read the two json files and then plot
+%read
+fname = 'G7_TAY93-User.output.json'; 
+fid = fopen(fname); 
+raw = fread(fid,inf); 
+str = char(raw'); 
+fclose(fid); 
+val2 = jsondecode(str);
+
+fname = 'G7_TAY93-Taylor.output.json'; 
+fid = fopen(fname); 
+raw = fread(fid,inf); 
+str = char(raw'); 
+fclose(fid); 
+val1 = jsondecode(str);
+
+cd ..
+
+%get the variables from it
+int1=val1.data.IRF.interest_.interest;
+ygap1=val1.data.IRF.interest_.outputgap;
+int2=val2.data.IRF.interest_.interest;
+ygap2=val2.data.IRF.interest_.outputgap;
+
+%plot 
+time=0:20;
+x_label_name="Quarters";
+y_label_int="Percentage Point Deviation";
+y_label_ygap="% Deviation";
+fontSize_axis=18;
+fontSize_title=20;
+fontSize_numbers=16;
+fontSize_legend=16;
+linewidth_curves=2;
+linewidth_zero=1;
+title_int="Interest";
+title_ygap="Output Gap";
+
+figure(1);
+plot(time, int1,'-b', 'Linewidth', linewidth_curves),hold on
+plot(time, int2,'-r', 'Linewidth', linewidth_curves),hold on
+plot(time,0.*int1,'-k','Linewidth', linewidth_zero),hold on
+%legend('G7\_TAY93, rule 1','G7\_TAY93, rule 2','')
+hold off
+ax = gca;
+ax.FontSize = fontSize_numbers;
+title(title_int,'FontSize', fontSize_title)
+xlabel(x_label_name,'FontSize', fontSize_axis)
+ylabel(y_label_int,'FontSize', fontSize_axis)
+figure(2);
+plot(time, ygap1,'-b', 'Linewidth', linewidth_curves),hold on
+plot(time, ygap2,'-r', 'Linewidth', linewidth_curves),hold on
+plot(time,0.*int1,'-k','Linewidth', linewidth_zero),hold on
+legend('Rule (1)','Rule (2)','','FontSize',fontSize_legend)
+hold off
+ax = gca;
+ax.FontSize = fontSize_numbers;
+title(title_ygap,'FontSize', fontSize_title)
+xlabel(x_label_name,'FontSize', fontSize_axis)
+ylabel(y_label_ygap,'FontSize', fontSize_axis)
+ylim([-0.2 0.02])
+%ylim(y_lim_value)
+
+%% Figure 4
+mmb('config_3.json','var');
+
+cd out
+
+%read the two json files and then plot
+%read
+fname = 'G7_TAY93-CEE.output.json'; 
+fid = fopen(fname); 
+raw = fread(fid,inf); 
+str = char(raw'); 
+fclose(fid); 
+val1 = jsondecode(str);
+
+fname = 'US_ACELm-CEE.output.json'; 
+fid = fopen(fname); 
+raw = fread(fid,inf); 
+str = char(raw'); 
+fclose(fid); 
+val2 = jsondecode(str);
+
+fname = 'US_SW07-CEE.output.json'; 
+fid = fopen(fname); 
+raw = fread(fid,inf); 
+str = char(raw'); 
+fclose(fid); 
+val3 = jsondecode(str);
+
+cd ..
+
+%get the variables from it
+int1=val1.data.IRF.interest_.interest;
+y1=val1.data.IRF.interest_.output;
+int2=val2.data.IRF.interest_.interest;
+y2=val2.data.IRF.interest_.output;
+int3=val3.data.IRF.interest_.interest;
+y3=val3.data.IRF.interest_.output;
+
+%plot 
+time=0:20;
+x_label_name="Quarters";
+y_label_int="Percentage Point Deviation";
+y_label_y="% Deviation";
+title_int="Interest";
+title_y="Output";
+
+figure(3);
+plot(time, int1,'-b', 'Linewidth', linewidth_curves),hold on
+plot(time, int2,'-r', 'Linewidth', linewidth_curves),hold on
+plot(time, int3,'-g', 'Linewidth', linewidth_curves),hold on
+plot(time,0.*int1,'-k','Linewidth', linewidth_zero),hold on
+%legend('G7\_TAY93, rule 1','G7\_TAY93, rule 2','')
+hold off
+ax = gca;
+ax.FontSize = fontSize_numbers;
+title(title_int,'FontSize', fontSize_title)
+xlabel(x_label_name,'FontSize', fontSize_axis)
+ylabel(y_label_int,'FontSize', fontSize_axis)
+%ylim([-0.25 1.25])
+figure(4);
+plot(time, y1,'-b', 'Linewidth', linewidth_curves),hold on
+plot(time, y2,'-r', 'Linewidth', linewidth_curves),hold on
+plot(time, y3,'-g', 'Linewidth', linewidth_curves),hold on
+plot(time,0.*int1,'-k','Linewidth', linewidth_zero),hold on
+legend('G7\_TAY93','US\_ACELm','US\_SW07','','FontSize',fontSize_legend)
+hold off
+ax = gca;
+ax.FontSize = fontSize_numbers;
+title(title_y,'FontSize', fontSize_title)
+xlabel(x_label_name,'FontSize', fontSize_axis)
+ylabel(y_label_y,'FontSize', fontSize_axis)
+%ylim([-0.4 0.1])
+%ylim(y_lim_value)
+
