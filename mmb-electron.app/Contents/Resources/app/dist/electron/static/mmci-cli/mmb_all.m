3acccba753e64e5806c98e9f4bcd4c846c641306
bankows <kristofer.bank@gmail.com>
2023-11-16 12:30:15 +0100

    Creating meta driver

---
+++ mmb-electron.app/Contents/Resources/app/dist/electron/static/mmci-cli/mmb_all.m
@@ -0,0 +1,11 @@
+% initiating the project
+clear all; close all; clc
+[projectPath, subProjectPath] = init();
+
+% running FiMod under various monetary policy rules
+mmb_FiModWithDiffRules
+
+% running various MMB models with SW rule
+% so that the results of FiMod could be put into 
+% perspective
+mmb_SWruleWithDiffModels
