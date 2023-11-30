865c30b30decc48dbcf96184b3a3ed3492c467cb
bankows <kristofer.bank@gmail.com>
2023-11-30 17:07:27 +0100

    Going back to the MMB compatible version of GEAR

--- mmb-electron.app/Contents/Resources/app/dist/electron/static/mmci-cli/models/DEREA_GEAR16/DEREA_GEAR16_steadystate.m
+++ mmb-electron.app/Contents/Resources/app/dist/electron/static/mmci-cli/models/DEREA_GEAR16/DEREA_GEAR16_steadystate.m
@@ -478,8 +478,8 @@ thetaw_b_t = thetaw_b;
    inflation = 0;
    inflationq = 0;
    outputgap = 0;
-   output = 0;
-   fispol = 0;
+   output = 100*(pop_a/(pop_b+pop_a)*log(y_a_ts)+(pop_b/(pop_b+pop_a)*log(y_b_ts)));;
+   fispol = cG_a_ts;
    
    
   endoList = M_.endo_names(~startsWith(M_.endo_names, "AUX_"));   
