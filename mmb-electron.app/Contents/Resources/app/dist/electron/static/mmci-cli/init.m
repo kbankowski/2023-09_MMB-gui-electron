a9aeff1796d7073b5db48cf189eaa084c0db2c5b
bankows <kristofer.bank@gmail.com>
2023-12-01 11:22:58 +0100

    Switching to the new version of Dynare

--- mmb-electron.app/Contents/Resources/app/dist/electron/static/mmci-cli/init.m
+++ mmb-electron.app/Contents/Resources/app/dist/electron/static/mmci-cli/init.m
@@ -1,10 +1,16 @@
 function [projectPath, subProjectPath, projectPathFimod] = init()
 
+    restoredefaultpath;
+
     iris_path = "/Users/kk/Documents/0000-00_work/2021-03_iris/iris_live";
 
     % Loading Iris
     addpath(iris_path);
     iris.startup
+
+    % Loading Dynare
+    addpath('/Applications/Dynare/5.5-arm64/matlab');
+    dynare_config()
 
     % Adding paths
     projectPath = '/Users/kk/Documents/0000-00_work/2023-09_MMB-gui-electron';
