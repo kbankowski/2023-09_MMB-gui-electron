7b4206a2812c9da519660198cad39e4ea99818f3
bankows <kristofer.bank@gmail.com>
2023-12-19 16:23:30 +0100

    Investigating fiscal contributions (adjusting the files)

--- mmb-electron.app/Contents/Resources/app/dist/electron/static/mmci-cli/mmb_fiscalContributions.m
+++ mmb-electron.app/Contents/Resources/app/dist/electron/static/mmci-cli/mmb_fiscalContributions.m
@@ -1,59 +1,72 @@
-%% PREAMBLE
-% clearing the workspace
-clear all; close all; clc;
-% Executing call_paths not tracked by GIT
-addpath('../../subroutines')
-call_paths;
-addpath(genpath(project_path));
-% Set up project name
-ProjectName = "WPfiscal";
-% Call our project environment
-envi = environment.WPfiscal.setup();
+%% Pre-amble
+clear all; close all; clc
+[projectPath, subProjectPath, projectPathFiMod, projectPathGEAR] = init();
 
-% Call Dynare
-addpath(dynare_5_1_official);
-dynare_config
-% Call Iris
-addpath(iris_path_20211222);
-iris.startup
+%% reading in models in Dynare
+mmb('config_2g.json','var');
 
-%% running standard shocks for Section 2
-CalcsDbRawGovDebtExo = WGEM_mf_report.runStndShocks(envi);
-save(sprintf('%s/databases/%s/OutDb_BASE_WGEMstndShocks_%s_%s.mat', project_path, char(ProjectName), "ShocksSpec", "MpEndo"), 'CalcsDbRawGovDebtExo');
+%% values simulated with simult_ Dynare function
+% the major advantage of this approach is that one can
+% explicitely see both ss values and shock-simulated values
 
-% plotting using the results and saving the figures in CSV files
-load(sprintf('%s/databases/%s/OutDb_BASE_WGEMstndShocks_%s_%s.mat', project_path, char(ProjectName), "ShocksSpec", "MpEndo"), 'CalcsDbRawGovDebtExo');
-plotting.WPfiscal.FSimulDeviationsReport(envi, CalcsDbRawGovDebtExo, ProjectName, "qrt");
-plotting.WPfiscal.FSimulDeviationsReport(envi, CalcsDbRawGovDebtExo, ProjectName, "ann");
+% creating shocks
+ex_ = databank.fromArray( ...
+    zeros(20, length(M_.exo_names)) ...
+    , M_.exo_names ...
+    , qq(1, 1) ...
+);
+ex_.interest_(qq(1, 1)) = 1;
 
-%% running gov investment shocks for Section 3
+% stochastic simulation
+y_ = simult_( ...
+    M_ ...
+    , options_ ...
+    , oo_.steady_state ...
+    , oo_.dr ...
+    , databank.toArray(ex_) ...
+    , 1 ...
+);
 
-% temporary shock
-WGEM_mf_report.runGovInvShocks(envi, "ShocksTemp", "MpEndo");
-% temporary shock with MP exogenous for 8 quarters
-WGEM_mf_report.runGovInvShocks(envi, "ShocksTemp", "MpExo");
-% permanent shock
-WGEM_mf_report.runGovInvShocks(envi, "ShocksPerm", "MpEndo");
+% stochastic simulation, SS and deviations as databanks
+aSeriesY_ = databank.fromArray( ...
+    y_' ...
+    , M_.endo_names ...
+    , qq(1, 1)-1: qq(5, 4) ...
+);
+aSeriesSS = databank.fromArray( ...
+    repmat(oo_.steady_state', 21, 1) ...
+    , M_.endo_names ...
+    , qq(1, 1)-1: qq(5, 4) ...
+);
+resDynareSimult_ = dbfun(@(x, y) x - y, aSeriesY_, aSeriesSS);
 
-% loading the results
-load(sprintf('%s/databases/%s/OutDb_BASE_WGEMGovInv_ShocksTemp_MpEndo.mat', project_path, ProjectName), 'CalcsDbRawGovDebtExo');
-PlotDb.Temp_MpEndo = CalcsDbRawGovDebtExo;
-load(sprintf('%s/databases/%s/OutDb_BASE_WGEMGovInv_ShocksTemp_MpExo.mat', project_path, ProjectName), 'CalcsDbRawGovDebtExo');
-PlotDb.Temp_MpExo = CalcsDbRawGovDebtExo;
-load(sprintf('%s/databases/%s/OutDb_BASE_WGEMGovInv_ShocksPerm_MpEndo.mat', project_path, ProjectName), 'CalcsDbRawGovDebtExo');
-PlotDb.Perm_MpEndo = CalcsDbRawGovDebtExo;
+%% calculating the contributions
+% we start the list with the variable that we try to decompose
+aItemList = ["output", "inflation", "consumption"];
 
-% figure
-plotting.WPfiscal.IRFs_shock_gin_WGEM(envi, "GovInv", PlotDb, ProjectName, "WGEMreport");
-
-% saving results in csv files
-for aSim = ["Temp_MpEndo", "Temp_MpExo", "Perm_MpEndo"]
-    databank.toCSV(...
-        databank.apply(@(x) x{qq(1, 1): qq(2015, 4)}, databank.redate(databank.copy(PlotDb.(aSim).GovInv.irf_dev, "SourceNames", envi.Meta.Lists.Variables.PlottingWGEM.irf_shocks(1:end-1)), qq(2822, 2), qq(1))) ...
-        , sprintf('%s/docu/%s/%s/csvFiles/%s.csv', project_path, ProjectName, "WGEMreport", aSim) ...
-        , Inf ...
-    );
+allItemList = aItemList(1);
+for aItem = aItemList
+    [ ...
+        contributionSeries.total.(aItem) ...
+        , contributionSeries.contrib.(aItem) ...
+        , contributionSeries.lhs.(aItem) ...
+        ] = subroutines.createContributions(char(aItem), aSeriesY_, aSeriesSS);
+    allItemList = [allItemList, contributionSeries.contrib.(aItem).Comment];
 end
+
+meta.allItemList = unique(allItemList);
+
+colorTable = table( ...
+    'Size',[numel(unique(allItemList)), 1] ...
+    , 'VariableTypes', {'cell'} ...
+    , 'VariableNames',{'colorIndex'} ...
+    , 'RowNames',unique(allItemList) ...
+    );
+colormapSaved = plotting.linspecer(numel(unique(allItemList)));
+for aItemIndex = 1:numel(unique(allItemList))
+    colorTable{aItemIndex, 1} = {colormapSaved(aItemIndex, :)};
+end
+contributionSeries.colorTable = colorTable;
 
 %% investigating interest rate reaction upon the request from Sandra
 load(sprintf('%s/databases/%s/OutDb_BASE_WGEMGovInv_ShocksTemp_MpExo.mat', project_path, ProjectName), 'CalcsDbRawGovDebtExo');
